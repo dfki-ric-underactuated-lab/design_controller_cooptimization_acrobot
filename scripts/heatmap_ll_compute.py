cabc3c434c4c511037697110b3597e7a60266774
felix.wiebe <felix.wiebe@dfki.de>
2022-07-22 22:12:40 +0200

    ll-heatmap scripts

---
+++ scripts/heatmap_ll_compute.py
@@ -0,0 +1,107 @@
+import sys
+import pickle
+import numpy as np
+import os
+import shutil
+import multiprocessing
+
+sys.path.append("../src")
+from coop.obj_fcts import caprr_coopt_interface
+
+
+filename = 'najafi_vols_ll.pickle'
+
+N_PROC = 2
+evals = 100000
+n = 32
+
+m1 = 0.608
+m2 = 0.2
+l1 = 0.2
+l2 = 0.4
+
+
+save_dir = os.path.join("..", "results", "myresults")
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+save_file = os.path.join(save_dir, filename)
+
+
+design_params = {
+    "m": [m1, m2],
+    "l": [l1, l2],
+    "lc": [l1, l2],
+    "b": [0.0, 0.0],
+    "fc": [0.0, 0.0],
+    "g": 9.81,
+    "I": [m1*l1**2, m2*l2**2],
+    "tau_max": [0.0, 5.0],
+}
+R_init = np.diag((0.94, 0.94))
+Q_init = np.diag((1.0, 1.0, 0.99, 0.99))
+
+caprr_najafi = caprr_coopt_interface(
+    design_params, Q_init, R_init, backend="najafi",
+    najafi_evals=evals, estimate_clbk=None)
+
+
+def roa(roa_interface, m2, l1, l2, idx1, idx2, save_dir):
+    vol = roa_interface.design_opt_obj([m2, l1, l2])
+    np.savetxt(os.path.join(save_dir, "roa_" +
+               str(idx1).zfill(2)+"_"+str(idx2).zfill(2)), [vol])
+
+
+data_dir = "parallel_data"
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+l1Vals = np.linspace(0.2, 0.4, n)
+l2Vals = np.linspace(0.2, 0.4, n)
+
+comp_list = []
+for idx1, l1 in enumerate(l1Vals):
+    for idx2, l2 in enumerate(l2Vals):
+        comp_list.append([l1, l2, idx1, idx2])
+
+comp_list2 = []
+cc = []
+for i, c in enumerate(comp_list):
+    if i > 0 and i % N_PROC == 0:
+        comp_list2.append(cc)
+        cc = []
+    cc.append(c)
+if len(cc) > 0:
+    comp_list2.append(cc)
+
+manager = multiprocessing.Manager()
+jobs = []
+for c in comp_list2:
+    for cc in c:
+        p = multiprocessing.Process(target=roa, args=(caprr_najafi,
+                                                      m2,
+                                                      cc[0],
+                                                      cc[1],
+                                                      cc[2],
+                                                      cc[3],
+                                                      data_dir))
+        jobs.append(p)
+        p.start()
+    for proc in jobs:
+        proc.join()
+
+prob_vols = np.zeros((len(l1Vals), len(l2Vals)))
+for idx1, q1 in enumerate(l1Vals):
+    for idx2, q2 in enumerate(l2Vals):
+        prob_vols[idx1][idx2] = -np.loadtxt(os.path.join(
+            data_dir, "roa_"+str(idx1).zfill(2)+"_"+str(idx2).zfill(2)))
+
+results = {"prob_vols": prob_vols,
+           "yticks": l1Vals,
+           "xticks": l2Vals}
+
+outfile = open(save_file, 'wb')
+# pickle.dump(prob_vols, outfile)
+pickle.dump(results, outfile)
+outfile.close()
+
+shutil.rmtree(data_dir)
---
+++ scripts/heatmap_ll_plot.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+load_dir = "../results/myresults"
+load_filename = 'najafi_vols_ll.pickle'
+load_file = os.path.join(load_dir, load_filename)
+
+save_filename = "ll_heatmap.pdf"
+save_file = os.path.join(load_dir, save_filename)
+
+
+mark = [0.4, 0.2]
+
+infile = open(load_file, 'rb')
+results = pickle.load(infile, encoding='bytes')
+prob_vols = -results["prob_vols"]
+yticks = results["yticks"]
+xticks = results["xticks"]
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.size": 26
+})
+
+fig = plt.figure(figsize=(8, 8))
+# left right bottom top
+plt.imshow(prob_vols, cmap='plasma', extent=[np.min(xticks), np.max(
+    xticks), np.min(yticks), np.max(yticks)], origin="lower")
+plt.colorbar(shrink=0.8)
+plt.scatter(mark[0], mark[1], s=200, color="green")
+plt.text(mark[0]-0.02, mark[1]+0.01, "1", color="green")
+plt.xlim(0.2, 0.4)
+plt.xticks([0.2, 0.25, 0.3, 0.35, 0.4])
+plt.xlabel("$l_2$ in $m$")
+plt.ylabel("$l_1$ in $m$")
+plt.savefig(save_file, bbox_inches="tight")
+plt.show()
