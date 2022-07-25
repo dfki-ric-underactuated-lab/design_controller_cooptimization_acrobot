aa0a3e7cb206fc64ff60331b4e97fac27e1fa7da
felix.wiebe <felix.wiebe@dfki.de>
2022-07-25 17:40:45 +0200

    similar structure for cmaes scripts

--- scripts/cmaes_altopt_cd.py
+++ scripts/cmaes_altopt_cd.py
@@ -1,14 +1,11 @@
 import sys
 import os
-from datetime import datetime
 
 sys.path.append("../src")
-from coop.roa_paropt import roa_alternate_opt
+from coop.cmaes_optimization import roa_alternate_opt
 
 
 init_pars = [1., 1., 1., 1., 1., 0.63, 0.3, 0.2]
-par_prefactors = [1., 1., 1., 1., 1.,
-                  1., 1., 1.]
 bounds = [[0.1, 10], [0.1, 10], [0.1, 1], [0.1, 1], [0.1, 1],
           [0.1, 1], [0.2, 0.4], [0.2, 0.4]]
 maxfevals_per_opt = 500
@@ -16,14 +13,12 @@ opt_order = ["c", "d"]
 roa_backend = "najafi"
 najafi_evals = 100
 robot = "acrobot"
-num_proc = 0
+num_proc = 2
 
 
-timestamp = datetime.today().strftime("%Y%m%d-%H%M%S")
-save_dir = os.path.join("data", robot, "lqr", "roa_altopt", timestamp+str("cd"))
+save_dir = os.path.join("../results", "myresults", "cmaes", "controller_first")
 
 best_par = roa_alternate_opt(init_pars=init_pars,
-                             par_prefactors=par_prefactors,
                              bounds=bounds,
                              maxfevals_per_opt=maxfevals_per_opt,
                              opt_order=opt_order,
--- scripts/cmaes_altopt_dc.py
+++ scripts/cmaes_altopt_dc.py
@@ -2,12 +2,10 @@ import sys
 import os
 
 sys.path.append("../src")
-from coop.roa_paropt import roa_alternate_opt
+from coop.cmaes_optimization import roa_alternate_opt
 
 
 init_pars = [1., 1., 1., 1., 1., 0.63, 0.3, 0.2]
-par_prefactors = [1., 1., 1., 1., 1.,
-                  1., 1., 1.]
 bounds = [[0.1, 10], [0.1, 10], [0.1, 1], [0.1, 1], [0.1, 1],
           [0.1, 1], [0.2, 0.4], [0.2, 0.4]]
 maxfevals_per_opt = 500
@@ -15,12 +13,11 @@ opt_order = ["d", "c"]
 roa_backend = "najafi"
 najafi_evals = 100000
 robot = "acrobot"
-num_proc = 0
+num_proc = 2
 
-save_dir = os.path.join("../results", "myresults", "cmaes", "design_controller")
+save_dir = os.path.join("../results", "myresults", "cmaes", "design_first")
 
 best_par = roa_alternate_opt(init_pars=init_pars,
-                             par_prefactors=par_prefactors,
                              bounds=bounds,
                              maxfevals_per_opt=maxfevals_per_opt,
                              opt_order=opt_order,
